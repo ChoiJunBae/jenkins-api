9d08794980b537f2cad85f21dbd2db6f014823e1
ChoiJunBae <cjb9610@naver.com>
2022-01-13 17:56:10 +0900

    first init

---
+++ src/test/java/com/example/jenkinsapi/JenkinsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
